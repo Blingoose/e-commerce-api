89e0380d3e1ea2e6e8449c0d2a46b261613623d5
Andre Katov <andy.katov@icloud.com>
2023-01-11 14:51:59 +0200

    testing register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,12 @@
 import asyncWrapper from "../middleware/asyncWrapper.js";
+import User from "../models/User.js";
+import customErrors from "../errors/error-index.js";
+import { StatusCodes } from "http-status-codes";
 
 const authControllers = {
   register: asyncWrapper(async (req, res, next) => {
-    res.send("Register user controller");
+    const user = await User.create(req.body);
+    res.status(StatusCodes.CREATED).json({ user });
   }),
 
   login: asyncWrapper(async (req, res, next) => {
