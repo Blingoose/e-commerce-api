75e977cfe1e744cb4896a243cc367a7866d5783f
Andre Katov <andy.katov@icloud.com>
2023-01-11 14:50:08 +0200

    added customError on duplicate email

--- middleware/error-handler-middleware.js
+++ middleware/error-handler-middleware.js
@@ -5,7 +5,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong, try again later.",
   };
+
+  if (err.code === 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = "Email already exist!";
+  }
   res.status(customError.statusCode).json({ errMsg: customError.msg });
+  // res.status(customError.statusCode).json({ err });
 };
 
 export default errorHandlerMiddleware;
