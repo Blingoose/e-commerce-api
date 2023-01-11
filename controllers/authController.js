48118d79ccb86d60f37ac386f60322b577a668ed
Andre Katov <andy.katov@icloud.com>
2023-01-11 19:21:29 +0200

    minor fix: preventing user registration as admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,9 @@ import { StatusCodes } from "http-status-codes";
 
 const authControllers = {
   register: asyncWrapper(async (req, res, next) => {
-    const user = await User.create(req.body);
+    const { name, email, password } = req.body;
+
+    const user = await User.create({ name, email, password });
     res.status(StatusCodes.CREATED).json({ user });
   }),
 
