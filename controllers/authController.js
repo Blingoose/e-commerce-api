c88e91c668092257445a28d7cb623581744acbdd
Andre Katov <andy.katov@icloud.com>
2023-01-12 22:38:25 +0200

    adding token when creating a user

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import asyncWrapper from "../middleware/asyncWrapper.js";
 import User from "../models/User.js";
 import customErrors from "../errors/error-index.js";
 import { StatusCodes } from "http-status-codes";
+import jwtHandler from "../utils/jwt.js";
 
 const authControllers = {
   register: asyncWrapper(async (req, res, next) => {
@@ -12,7 +13,11 @@ const authControllers = {
     const role = isFirstAccount ? "admin" : "user";
 
     const user = await User.create({ name, email, password, role });
-    res.status(StatusCodes.CREATED).json({ user });
+    const tokenUser = { name: user.name, userId: user._id, role: user.role };
+
+    const token = jwtHandler.createJWT({ payload: tokenUser });
+
+    res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
   }),
 
   login: asyncWrapper(async (req, res, next) => {
