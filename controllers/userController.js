2e60f6a40f1384489e970aa70e0f31de002167ff
Andre Katov <andy.katov@icloud.com>
2023-01-14 22:43:50 +0200

    testing via postman

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,16 +6,16 @@ const userControllers = {
     res.send("get all users route");
   }),
   getSingleUser: asyncWrapper(async (req, res, next) => {
-    res.send("get single user route");
+    res.send(req.params);
   }),
   showCurrentUser: asyncWrapper(async (req, res, next) => {
     res.send("show current user route");
   }),
   updateUser: asyncWrapper(async (req, res, next) => {
-    res.send("update user route");
+    res.send(req.body);
   }),
   updateUserPassword: asyncWrapper(async (req, res, next) => {
-    res.send("update user password route");
+    res.send(req.body);
   }),
 };
 
