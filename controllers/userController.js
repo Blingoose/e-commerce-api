24a2e0ca5723b6bec7745422f30a34de824962d9
Andre Katov <andy.katov@icloud.com>
2023-01-14 23:32:08 +0200

    added functionality: getAllUsers, getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,35 @@
-import CustomErrors from "../errors/error-index.js";
+import User from "../models/User.js";
 import asyncWrapper from "../middleware/asyncWrapper.js";
+import CustomErrors from "../errors/error-index.js";
+import { StatusCodes } from "http-status-codes";
 
 const userControllers = {
   getAllUsers: asyncWrapper(async (req, res, next) => {
-    res.send("get all users route");
+    // find the user but exclude password from the user data.
+    const users = await User.find({ role: "user" }).select("-password");
+    if (users.length === 0) {
+      throw new CustomErrors.NotFoundError("No users in database");
+    }
+    res.status(StatusCodes.OK).json({ users });
   }),
+
   getSingleUser: asyncWrapper(async (req, res, next) => {
-    res.send(req.params);
+    const { id: userId } = req.params;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      throw new CustomErrors.NotFoundError(`No user with id: ${userId}`);
+    }
+    res.status(StatusCodes.OK).json({ user });
   }),
+
   showCurrentUser: asyncWrapper(async (req, res, next) => {
     res.send("show current user route");
   }),
+
   updateUser: asyncWrapper(async (req, res, next) => {
     res.send(req.body);
   }),
+
   updateUserPassword: asyncWrapper(async (req, res, next) => {
     res.send(req.body);
   }),
