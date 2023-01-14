bcb08d6068bd4922cfb63945095d9f661359fd68
Andre Katov <andy.katov@icloud.com>
2023-01-14 22:36:09 +0200

    minor fix: update routes should be patch, not post

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,10 @@ const userRouter = express.Router();
 
 userRouter.route("/").get(userControllers.getAllUsers);
 userRouter.route("/showMe").get(userControllers.showCurrentUser);
-userRouter.route("/updateUser").post(userControllers.updateUser);
+userRouter.route("/updateUser").patch(userControllers.updateUser);
 userRouter
   .route("/updateUserPassword")
-  .post(userControllers.updateUserPassword);
+  .patch(userControllers.updateUserPassword);
 
 // dynamic route, keep on bottom
 userRouter.route("/:id").get(userControllers.getSingleUser);
