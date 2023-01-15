02f1d91dacf01ae140bfcb1f2690d3d3570e588a
Andre Katov <andy.katov@icloud.com>
2023-01-15 16:28:01 +0200

    added user authentication functinality

---
+++ middleware/authentication-middleware.js
@@ -0,0 +1,26 @@
+import CustomErrors from "../errors/error-index.js";
+import { StatusCodes } from "http-status-codes";
+import jwtHandler from "../utils/jwt.js";
+import asyncWrapper from "./asyncWrapper.js";
+
+const authenticateUser = asyncWrapper(async (req, res, next) => {
+  const token = req.signedCookies.token;
+
+  if (!token) {
+    throw new CustomErrors.UnauthorizedError("Authentication invalid");
+  }
+  try {
+    const payload = jwtHandler.isTokenValid({ token });
+    req.user = {
+      userId: payload.userId,
+      name: payload.name,
+      role: payload.role,
+      expiration: new Date(payload.exp * 1000).toLocaleString(),
+    };
+    next();
+  } catch (error) {
+    throw new CustomErrors.UnauthorizedError("Authentication invalid");
+  }
+});
+
+export default authenticateUser;
