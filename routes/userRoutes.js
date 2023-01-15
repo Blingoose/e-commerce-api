cffafde3c3a78f5e8248a8ebab81fd0024d55bdd
Andre Katov <andy.katov@icloud.com>
2023-01-15 16:29:25 +0200

    added authenticateUser to routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import userControllers from "../controllers/userController.js";
 import express from "express";
+import authenticateUser from "../middleware/authentication-middleware.js";
 
 const userRouter = express.Router();
 
-userRouter.route("/").get(userControllers.getAllUsers);
+userRouter.route("/").get(authenticateUser, userControllers.getAllUsers);
 userRouter.route("/showMe").get(userControllers.showCurrentUser);
 userRouter.route("/updateUser").patch(userControllers.updateUser);
 userRouter
@@ -11,6 +12,6 @@ userRouter
   .patch(userControllers.updateUserPassword);
 
 // dynamic route, keep on bottom
-userRouter.route("/:id").get(userControllers.getSingleUser);
+userRouter.route("/:id").get(authenticateUser, userControllers.getSingleUser);
 
 export default userRouter;
