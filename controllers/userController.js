c3d1d296069cbb188bd6c05440186e6afc8b4741
Andre Katov <andy.katov@icloud.com>
2023-01-17 00:00:30 +0200

    refactor updateUser using user.save()

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,32 +33,60 @@ const userControllers = {
     if (!name || !email) {
       throw new CustomErrors.BadRequestError("Must provide name and email");
     }
-    const user = await User.findOneAndUpdate(
-      { _id: req.user.userId },
-      { $set: { email, name } },
-      { new: true, runValidators: true }
-    );
+
+    const user = await User.findOne({ _id: req.user.userId });
+
+    user.email = email;
+    user.name = name;
+
+    await user.save();
+
     const tokenUser = jwtHandler.createTokenUser(user);
     jwtHandler.attachCookiesToResponse({ res, user: tokenUser });
+
     res.status(StatusCodes.OK).json({ user: tokenUser });
   }),
 
   updateUserPassword: asyncWrapper(async (req, res, next) => {
     const { oldPassword, newPassword } = req.body;
+
     if (!oldPassword || !newPassword) {
       throw new CustomErrors.BadRequestError(
         "You must provide old password and new password"
       );
     }
+
     const user = await User.findById(req.user.userId);
     const isOldPasswordMatch = await user.comparePassword(oldPassword);
+
     if (isOldPasswordMatch === false) {
       throw new CustomErrors.UnauthorizedError("old password is invalid");
     }
+
     user.password = newPassword;
     await user.save();
+
     res.status(StatusCodes.OK).json({ msg: "Success! password updated" });
   }),
 };
 
 export default userControllers;
+
+//! Reference for using findOneAndUpdate, which doesn't triggers the pre("save") in the User module.
+// updateUser: asyncWrapper(async (req, res, next) => {
+//   const { name, email } = req.body;
+//   if (!name || !email) {
+//     throw new CustomErrors.BadRequestError("Must provide name and email");
+//   }
+
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.user.userId },
+//     { $set: { email, name } },
+//     { new: true, runValidators: true }
+//   );
+
+//   const tokenUser = jwtHandler.createTokenUser(user);
+//   jwtHandler.attachCookiesToResponse({ res, user: tokenUser });
+
+//   res.status(StatusCodes.OK).json({ user: tokenUser });
+// })
