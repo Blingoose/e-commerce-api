a9fe8a9c712d601b0c0efb088db6a9c27e1726a0
Andre Katov <andy.katov@icloud.com>
2023-01-17 21:56:23 +0200

    refactor utils: added validatorMixMax

--- utils/utils.js
+++ utils/utils.js
@@ -1,26 +1,17 @@
-// find last occurance of comma in an array and replace it with "and"
-// Usage exmple: a,b,c will become a,b and c.
-export const replaceLastCommaWithAnd = (arr) => {
-  const fieldToString = arr.join(", ");
-  //! Explanation: /,(?=[^,]*$)/  I.E find the last comma in a string.
-  //! this regex matches any comma at the end of a string, which is followed by zero or more characters which are not a comma.
-  const replaceLastComma = fieldToString.replace(/,(?=[^,]*$)/, " and");
-
-  console.log(replaceLastComma);
-  return replaceLastComma;
-};
-
-export const checkIfArrayHasMoreThanOne = (arr) => {
-  const isPlural = arr.length > 1 ? true : false;
-  return isPlural;
-};
-
-export const checkIfWordStartsWithVowel = (word) => {
-  const vowel = "aeiou";
-  if (Array.isArray(word)) {
-    const firstCharOfFirstWord = word[0].charAt(0).toLowerCase();
-    return vowel.indexOf(firstCharOfFirstWord) !== -1 ? true : false;
-  } else {
-    return vowel.indexOf(word.charAt(0)) !== -1 ? true : false;
-  }
+export const validatorMinMax = (validationType, validationValue) => {
+  return {
+    validator: (val) => {
+      if (validationType === "minlength") {
+        return val.length >= validationValue;
+      } else if (validationType === "maxlength") {
+        return val.length <= validationValue;
+      }
+    },
+    message: ({ value }) =>
+      `${
+        validationType === "minlength" ? "Minimum" : "Maximum"
+      } length of ${validationValue} characters. You've entered ${
+        value.length
+      } characters`,
+  };
 };
