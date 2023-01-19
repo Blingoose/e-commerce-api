d3a053b2620bb1633bb95d20f44161ab4ec80f59
Andre Katov <andy.katov@icloud.com>
2023-01-19 18:29:32 +0200

    added uploadImage functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,11 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import Product from "../models/product.js";
 import asyncWrapper from "../middleware/asyncWrapper.js";
 import { StatusCodes } from "http-status-codes";
 import CustomErrors from "../errors/error-index.js";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const productControllers = {
   createProduct: asyncWrapper(async (req, res, next) => {
@@ -29,14 +33,16 @@ const productControllers = {
 
   deleteProduct: asyncWrapper(async (req, res, next) => {
     const { id: productId } = req.params;
-    const removedProduct = await Product.findByIdAndRemove(productId);
+    const product = await Product.findById(productId);
 
-    if (!removedProduct) {
+    if (!product) {
       throw new CustomErrors.NotFoundError(
         `No product with id: ${productId} found`
       );
     }
-    res.status(StatusCodes.OK).json({ removedProduct });
+    await product.remove();
+
+    res.status(StatusCodes.OK).json({ removed: product });
   }),
 
   updateProduct: asyncWrapper(async (req, res, next) => {
@@ -46,11 +52,39 @@ const productControllers = {
       new: true,
       runValidators: true,
     });
+
+    if (!product) {
+      throw new CustomErrors.NotFoundError(
+        `No product with id: ${productId} found`
+      );
+    }
+
     res.status(StatusCodes.OK).json({ product });
   }),
 
   uploadImage: asyncWrapper(async (req, res, next) => {
-    res.status(StatusCodes.OK).json({ msg: "upload image route" });
+    // console.log(req.files);
+    if (!req.files) {
+      throw new CustomErrors.BadRequestError("No file uploaded");
+    }
+
+    const productImage = req.files.image;
+    if (!productImage.mimetype.startsWith("image")) {
+      throw new CustomErrors.BadRequestError("Please upload image");
+    }
+
+    const maxSize = 1000 * 1000 * 7;
+    if (productImage.size > maxSize) {
+      throw new CustomErrors.BadRequestError("Image too big, max size is 7 MB");
+    }
+
+    const imagePath = path.join(
+      __dirname,
+      `../public/uploads/${productImage.name}`
+    );
+    await productImage.mv(imagePath);
+
+    res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
   }),
 };
 
