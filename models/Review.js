7ac4277a76f4d66a3b28e49c5b68abe2868a78de
Andre Katov <andy.katov@icloud.com>
2023-01-19 21:26:31 +0200

    fixed minor issue: rating validation

---
+++ models/Review.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import { ratingMinMax, validatorMinMax } from "../utils/utils.js";
+
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      validate: [ratingMinMax("min", 1), ratingMinMax("max", 5)],
+      required: [true, "Please provide value"],
+    },
+
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Pleae provide value"],
+      validate: validatorMinMax("maxlength", 80),
+    },
+
+    comment: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide value"],
+      validate: validatorMinMax("maxlength", 200),
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//making sure a user can only leave one review per product.
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+const Review = mongoose.model("Review", ReviewSchema);
+
+export default Review;
