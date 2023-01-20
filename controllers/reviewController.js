2f265df08e83a986b4ce5558c84764887781330e
Andre Katov <andy.katov@icloud.com>
2023-01-20 17:04:18 +0200

    refactored create review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,28 +1,64 @@
 import Review from "../models/Review.js";
+import Product from "../models/product.js";
 import asyncWrapper from "../middleware/asyncWrapper.js";
 import CustomErrors from "../errors/error-index.js";
 import { StatusCodes } from "http-status-codes";
 
 const reviewControllers = {
   createReview: asyncWrapper(async (req, res, next) => {
-    const { id: productId } = req.params;
     const { userId } = req.user;
+    const { product: productId } = req.body;
 
-    if (!productId) {
+    const isValidProduct = await Product.findById(productId);
+
+    if (!isValidProduct) {
       throw new CustomErrors.NotFoundError(`No product with id: ${productId}`);
     }
 
-    req.body.user = userId;
-    req.body.product = productId;
+    const alreadySubmitted = Review.findOne({
+      product: productId,
+      user: userId,
+    });
 
+    if (alreadySubmitted) {
+      throw new CustomErrors.BadRequestError(
+        "Already submitted review for this product"
+      );
+    }
+
+    req.body.user = userId;
     const review = await Review.create(req.body);
 
     res.status(StatusCodes.CREATED).json({ review });
   }),
 
-  getAllReviews: asyncWrapper(async (req, res, next) => {}),
+  getAllReviews: asyncWrapper(async (req, res, next) => {
+    const reviews = await Review.find({});
 
-  getSingleReview: asyncWrapper(async (req, res, next) => {}),
+    if (reviews.length === 0) {
+      throw new CustomErrors.NotFoundError("There are no reviews");
+    }
+
+    res.status(StatusCodes.OK).json({ reviews });
+  }),
+
+  getSingleReview: asyncWrapper(async (req, res, next) => {
+    const { id: productId } = req.params;
+
+    if (!productId) {
+      throw new CustomErrors.BadRequestError(
+        `No product with id: ${productId}`
+      );
+    }
+
+    const review = await Review.findOne({ product: productId });
+
+    if (!review) {
+      throw new CustomErrors.NotFoundError("No reviews found for this product");
+    }
+
+    res.status(StatusCodes.OK).json({ review });
+  }),
 
   deleteReview: asyncWrapper(async (req, res, next) => {}),
 
