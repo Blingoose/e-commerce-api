28d4163d818c9219ed9bff0da3e3759e228e971b
Andre Katov <andy.katov@icloud.com>
2023-01-20 17:03:09 +0200

    fixed wrong post route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,7 +5,14 @@ import reviewControllers from "../controllers/reviewController.js";
 const reviewRouter = express.Router();
 
 reviewRouter
-  .route("/:id")
+  .route("/")
+  .get(reviewControllers.getAllReviews)
   .post(authenticateUser, reviewControllers.createReview);
+
+reviewRouter
+  .route("/:id")
+  .get(reviewControllers.getSingleReview)
+  .patch(authenticateUser, reviewControllers.updateReview)
+  .delete(authenticateUser, reviewControllers.deleteReview);
 
 export default reviewRouter;
