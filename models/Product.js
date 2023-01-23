37a0b45532f116a1b7896499df2b7013d9939c4f
Andre Katov <andy.katov@icloud.com>
2023-01-23 21:43:28 +0200

    added numOfReviews property

--- models/Product.js
+++ models/Product.js
@@ -69,6 +69,11 @@ const ProductSchema = new mongoose.Schema(
       default: 0,
     },
 
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
