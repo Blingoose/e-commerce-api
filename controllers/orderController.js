70d22c93b9c0d8d51492d82a2df57cc99a55d387
Andre Katov <andy.katov@icloud.com>
2023-01-26 00:41:23 +0200

    added create order controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,11 +1,94 @@
 import Order from "../models/order.js";
 import Product from "../models/product.js";
-import asyncWrapper from "../middleware/asyncWrapper.js";
-import { StatusCodes } from "http-status-codes";
 import CustomErrors from "../errors/error-index.js";
+import asyncWrapper from "../middleware/asyncWrapper.js";
 import checkPersmissions from "../utils/checkPermissions.js";
+import { v4 } from "uuid";
+import { StatusCodes } from "http-status-codes";
+
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const currencyConverter = {
+    usd: amount / 100,
+    ils: amount * 0.033791,
+    eur: amount * 0.0091615,
+  };
+
+  if (!currencyConverter.hasOwnProperty(currency)) {
+    throw new CustomErrors.BadRequestError("Wrong currency");
+  }
+
+  const convertedAmount = currencyConverter[currency];
+
+  const client_secret = v4();
+  return { client_secret, amount: convertedAmount, currency };
+};
 
 const orderControllers = {
+  createOrder: asyncWrapper(async (req, res, next) => {
+    const { cartItems, tax, shippingFee } = req.body;
+
+    if (!cartItems || cartItems.length < 1) {
+      throw new CustomErrors.BadRequestError("No cart items provided");
+    }
+
+    if (!tax || !shippingFee) {
+      throw new CustomErrors.BadRequestError(
+        "Please provide tax and shippingFee values"
+      );
+    }
+
+    let orderItems = [];
+    let subtotal = 0;
+
+    for (const item of cartItems) {
+      const product = await Product.findById(item.product);
+
+      if (!product) {
+        throw new CustomErrors.NotFoundError(
+          `No product with id: ${item.product}`
+        );
+      }
+      const { name, price, image, _id } = product;
+
+      const singleOrderItem = {
+        amount: item.amount,
+        name,
+        price,
+        image,
+        product: _id,
+      };
+
+      // add item to order
+      orderItems = [...orderItems, singleOrderItem];
+      subtotal += item.amount * price;
+    }
+    const total = tax + shippingFee + subtotal;
+
+    //get client secret
+    const paymentIntent = await fakeStripeAPI({
+      amount: total,
+      currency: "ils",
+    });
+
+    const order = await Order.create({
+      orderItems,
+      total,
+      subtotal,
+      tax,
+      shippingFee,
+      clientSecret: paymentIntent.client_secret,
+      user: req.user.userId,
+    });
+
+    res.status(StatusCodes.CREATED).json({
+      order,
+      clientSecret: order.clientSecret,
+      currency: paymentIntent.currency,
+      [`converted total to ${paymentIntent.currency}`]:
+        paymentIntent.amount.toFixed(2),
+    });
+  }),
+
   getAllOrders: asyncWrapper(async (req, res, next) => {
     res.send("getAllOrders controller");
   }),
@@ -16,20 +99,6 @@ const orderControllers = {
 
   getCurrentUserOrders: asyncWrapper(async (req, res, next) => {
     res.send("getCurrentUserOrders controller");
-  }),
-
-  createOrder: asyncWrapper(async (req, res, next) => {
-    const { cartItems, tax, shippingFee } = req.body;
-
-    if (!cartItems || cartItems.length < 1) {
-      throw new CustomErrors.BadRequestError("No cart items provided");
-    }
-
-    if (!tax || !shippingFee) {
-      throw new CustomErrors.BadRequestError("Please provide value");
-    }
-
-    res.send("createOrder controller");
   }),
 
   updateOrder: asyncWrapper(async (req, res, next) => {
