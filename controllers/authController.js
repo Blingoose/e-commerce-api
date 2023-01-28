8fb6fa462d96233cf933d023fbfedea97a1f3691
Andre Katov <andy.katov@icloud.com>
2023-01-29 01:03:36 +0200

    email validation on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ import User from "../models/user.js";
 import CustomErrors from "../errors/error-index.js";
 import { StatusCodes } from "http-status-codes";
 import jwtHandler from "../utils/jwt.js";
+import joi from "joi";
 
 const authControllers = {
   register: asyncWrapper(async (req, res, next) => {
@@ -22,13 +23,15 @@ const authControllers = {
 
   login: asyncWrapper(async (req, res, next) => {
     const { email, password } = req.body;
-    if (!email || !password) {
-      throw new CustomErrors.BadRequestError(
-        "Please provide password and email"
-      );
+    if (!password) {
+      throw new CustomErrors.UnauthorizedError("Must provide password");
     }
+    const validateFields = new User({ email, password });
+    await validateFields.validate({ pathsToSkip: ["name", "password"] });
 
     const user = await User.findOne({ email });
+    console.log(user);
+
     if (!user) {
       throw new CustomErrors.UnauthorizedError(`User ${email} doesn't exist`);
     }
