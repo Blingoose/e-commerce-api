fcdd0c99817f6552b3cf79d496a8600e4b702a62
Andre Katov <andy.katov@icloud.com>
2023-01-29 21:36:04 +0200

    minor functionality tweak

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,11 +1,15 @@
 import CustomErrors from "../errors/error-index.js";
-const checkPermission = (requestUser, resourceUserId) => {
+const checkPermission = (requestUser, resourceUserId, itemId) => {
   //   console.log(requestUser.userId);
   //   console.log(resourceUserId);
   if (requestUser.role !== "admin" && requestUser.userId !== resourceUserId) {
-    throw new CustomErrors.NotFoundError(
-      `No item found with id: ${resourceUserId}`
-    );
+    if (itemId) {
+      throw new CustomErrors.NotFoundError(`No item found with id: ${itemId}`);
+    } else {
+      throw new CustomErrors.NotFoundError(
+        `No item found with id: ${resourceUserId}`
+      );
+    }
   }
 };
 
