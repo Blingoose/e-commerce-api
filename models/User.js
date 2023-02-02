4764c9511170933fb5f78ba2bb1e03cde9e98d55
Andre Katov <andy.katov@icloud.com>
2023-02-03 00:00:21 +0200

    added functionality for when user updates username

--- models/User.js
+++ models/User.js
@@ -6,11 +6,25 @@ import { validatorMinMax } from "../utils/utils.js";
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
+    trim: true,
     required: [true, "Value must be provided"],
     validate: [
       validatorMinMax("minlength", 3),
       validatorMinMax("maxlength", 50),
     ],
+    set: (val) => val.toLowerCase(),
+  },
+
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, "Value must be provided"],
+    validate: [
+      validatorMinMax("minlength", 4),
+      validatorMinMax("maxlength", 20),
+    ],
+    set: (val) => val.toLowerCase(),
   },
 
   email: {
@@ -21,10 +35,12 @@ const UserSchema = new mongoose.Schema({
       validator: validator.isEmail,
       message: "Please provide a valid email",
     },
+    set: (val) => val.toLowerCase(),
   },
 
   password: {
     type: String,
+    trim: true,
     required: [true, "Value must be provided"],
     validate: validatorMinMax("minlength", 6),
   },
@@ -59,11 +75,29 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre("save", async function () {
-  // console.log(this.modifiedPaths());
   if (this.isModified("password")) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
   }
+  // if a user changes his username, update all associated following & followers with that new username for all users.
+  // happens only for when a user modifies the path username, except for when registering.
+  if (this.isModified("username") && !this.isNew) {
+    const previousUsername = (await User.findById(this._id)).username;
+
+    // Update followers array
+    await User.updateMany(
+      { followers: previousUsername },
+      { $set: { followers: this.username } }
+    );
+
+    // Update following array
+    await User.updateMany(
+      { following: previousUsername },
+      { $set: { following: this.username } }
+    );
+  }
+
+  //TODO -------------------> When a user changes his username, update all associated reviews posted by him, with that new username <---------------------- TODO
 });
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
@@ -74,13 +108,51 @@ UserSchema.methods.comparePassword = async function (candidatePassword) {
 UserSchema.pre("remove", async function () {
   //Remove all reviews that are associated with that user to be deleted.
   await this.model("Review").deleteMany({ product: this._id });
-  // Remove the deleted user's id from other users' followers array
+
+  // Remove the deleted username from all associated followers & following users.
+  // Update the countFollowers & countFollowing for all users who were associated with the deleted username.
+  const update = {
+    $pull: {
+      followers: this.username,
+      following: this.username,
+    },
+  };
+
+  const followersSize = await User.aggregate([
+    { $match: { followers: this.username } },
+    { $project: { size: { $size: "$followers" } } },
+  ]);
+
+  const followingSize = await User.aggregate([
+    { $match: { following: this.username } },
+    { $project: { size: { $size: "$following" } } },
+  ]);
+
+  if (followersSize.length > 0) {
+    update.$inc = {
+      countFollowers: -followersSize[0].size,
+    };
+  }
+
+  if (followingSize.length > 0) {
+    update.$inc = {
+      ...update.$inc,
+      countFollowing: -followingSize[0].size,
+    };
+  }
+
   await User.updateMany(
-    { followers: this._id },
     {
-      $pull: { followers: this._id.toString() },
-      $inc: { countFollowers: -1 },
-    }
+      $or: [
+        {
+          followers: this.username,
+        },
+        {
+          following: this.username,
+        },
+      ],
+    },
+    update
   );
 });
 
