7761f65383c8c02820097f165c6ac452c729fde8
Andre Katov <andy.katov@icloud.com>
2023-02-02 23:52:52 +0200

    refactor: utilizing username instead of userId

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ import CustomErrors from "../errors/error-index.js";
 import { StatusCodes } from "http-status-codes";
 import jwtHandler from "../utils/jwt.js";
 import { excludeFields } from "../utils/utils.js";
+import checkPermission from "../utils/checkPermissions.js";
 
 const userControllers = {
   getAllUsers: asyncWrapper(async (req, res, next) => {
@@ -13,7 +14,7 @@ const userControllers = {
     //find all users who has role of "user", except for the current user.
     const users = await User.find({
       role: "user",
-      _id: { $nin: req.user.userId },
+      username: { $nin: req.user.username },
     }).select(exclude);
 
     if (users.length === 0) {
@@ -23,41 +24,29 @@ const userControllers = {
   }),
 
   getSingleUser: asyncWrapper(async (req, res, next) => {
-    const { id: userId } = req.params;
-    const { role, userId: currentUserId } = req.user;
-    const exclude = excludeFields(role, currentUserId, userId);
-    const user = await User.findById(userId).select(exclude);
+    const { id: username } = req.params;
+    const { role, currentUsername } = req.user;
+    const exclude = excludeFields(role, currentUsername, username);
+    const user = await User.findOne({ username: username }).select(exclude);
 
     if (!user) {
-      throw new CustomErrors.NotFoundError(`No item found with id: ${user}`);
+      throw new CustomErrors.NotFoundError(
+        `No user found with username: ${username}`
+      );
     }
     res.status(StatusCodes.OK).json({ user });
   }),
 
   showCurrentUser: asyncWrapper(async (req, res, next) => {
     const exclude = excludeFields("admin");
-    const currentUser = await User.findById(req.user.userId).select(exclude);
+    const currentUser = await User.findOne({
+      username: req.user.username,
+    }).select(exclude);
 
-    res.status(StatusCodes.OK).json({ user: currentUser });
-  }),
-
-  updateUser: asyncWrapper(async (req, res, next) => {
-    const { name, email } = req.body;
-    if (!name || !email) {
-      throw new CustomErrors.BadRequestError("Must provide name and email");
+    if (!currentUser) {
+      throw new CustomErrors.NotFoundError("Please log-in first");
     }
-
-    const user = await User.findOne({ _id: req.user.userId });
-
-    user.email = email;
-    user.name = name;
-
-    await user.save();
-
-    const tokenUser = jwtHandler.createTokenUser(user);
-    jwtHandler.attachCookiesToResponse({ res, user: tokenUser });
-
-    res.status(StatusCodes.OK).json({ user: tokenUser });
+    res.status(StatusCodes.OK).json({ user: currentUser });
   }),
 
   updateUserPassword: asyncWrapper(async (req, res, next) => {
@@ -69,7 +58,7 @@ const userControllers = {
       );
     }
 
-    const user = await User.findById(req.user.userId);
+    const user = await User.findOne({ username: req.user.username });
     const isOldPasswordMatch = await user.comparePassword(oldPassword);
 
     if (isOldPasswordMatch === false) {
@@ -83,47 +72,60 @@ const userControllers = {
   }),
 
   deleteUser: asyncWrapper(async (req, res, next) => {
-    const { id: userId } = req.params;
+    const { id: username } = req.params;
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      throw new CustomErrors.NotFoundError(
+        `No user found with username: ${username}`
+      );
+    }
+
+    checkPermission(req.user, user._id.toString(), username);
+    await user.remove();
+
+    res
+      .status(StatusCodes.OK)
+      .json(`User ${username} had been successfully removed`);
   }),
 
   followUser: asyncWrapper(async (req, res, next) => {
-    const { id: userToFollowId } = req.params;
+    const { id: usernameToFollow } = req.params;
 
-    const userToFollow = await User.findById(userToFollowId);
+    const userToFollow = await User.findOne({ username: usernameToFollow });
     if (!userToFollow) {
       throw new CustomErrors.NotFoundError(
-        `No item found with id: ${userToFollowId}`
+        `No user found with username: ${usernameToFollow}`
       );
     }
 
     const existingFollow = await User.findOne({
-      _id: req.user.userId,
-      following: { $in: [userToFollowId] },
+      username: req.user.username,
+      following: { $in: [usernameToFollow] },
     });
 
     if (existingFollow) {
       throw new CustomErrors.BadRequestError("You already following this user");
     }
 
-    if (req.user.userId === userToFollowId) {
+    if (req.user.username === usernameToFollow) {
       throw new CustomErrors.BadRequestError("You cannot follow yourself");
     }
 
     const bulkWrite = [
       {
         updateOne: {
-          filter: { _id: userToFollowId },
+          filter: { username: usernameToFollow },
           update: {
-            $addToSet: { followers: req.user.userId },
+            $addToSet: { followers: req.user.username },
             $inc: { countFollowers: 1 },
           },
         },
       },
       {
         updateOne: {
-          filter: { _id: req.user.userId },
+          filter: { username: req.user.username },
           update: {
-            $addToSet: { following: userToFollowId },
+            $addToSet: { following: usernameToFollow },
             $inc: { countFollowing: 1 },
           },
         },
@@ -134,22 +136,22 @@ const userControllers = {
 
     res
       .status(StatusCodes.OK)
-      .json({ msg: `Started following ${userToFollow.name}` });
+      .json({ msg: `Started following ${userToFollow.username}` });
   }),
 
   unfollowUser: asyncWrapper(async (req, res, next) => {
-    const { id: userToUnfollowId } = req.params;
+    const { id: usernameToUnfollow } = req.params;
 
-    const userToUnfollow = await User.findById(userToUnfollowId);
+    const userToUnfollow = await User.findOne({ username: usernameToUnfollow });
     if (!userToUnfollow) {
       throw new CustomErrors.NotFoundError(
-        `No item found with id: ${userToUnfollowId}`
+        `No user found with username: ${usernameToUnfollow}`
       );
     }
 
     const existingFollow = await User.findOne({
-      _id: req.user.userId,
-      following: { $in: [userToUnfollowId] },
+      username: req.user.username,
+      following: { $in: [usernameToUnfollow] },
     });
 
     if (!existingFollow) {
@@ -159,9 +161,9 @@ const userControllers = {
     const bulkWrite = [
       {
         updateOne: {
-          filter: { _id: userToUnfollowId },
+          filter: { username: usernameToUnfollow },
           update: {
-            $pull: { followers: req.user.userId },
+            $pull: { followers: req.user.username },
             $inc: { countFollowers: -1 },
           },
         },
@@ -169,9 +171,9 @@ const userControllers = {
 
       {
         updateOne: {
-          filter: { _id: req.user.userId },
+          filter: { username: req.user.username },
           update: {
-            $pull: { following: userToUnfollowId },
+            $pull: { following: usernameToUnfollow },
             $inc: { countFollowing: -1 },
           },
         },
@@ -182,7 +184,7 @@ const userControllers = {
 
     res
       .status(StatusCodes.OK)
-      .json({ msg: `You unfollowed ${userToUnfollow.name}` });
+      .json({ msg: `You unfollowed ${userToUnfollow.username}` });
   }),
 };
 
