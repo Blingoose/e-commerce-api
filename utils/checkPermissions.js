bb9db4ab9a03ac2483700cca38ff84409654bf38
Andre Katov <andy.katov@icloud.com>
2023-02-02 23:47:54 +0200

    minor change: itemId ---> item

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,10 +1,10 @@
 import CustomErrors from "../errors/error-index.js";
-const checkPermission = (requestUser, resourceUserId, itemId) => {
+const checkPermission = (requestUser, resourceUserId, item) => {
   //   console.log(requestUser.userId);
   //   console.log(resourceUserId);
   if (requestUser.role !== "admin" && requestUser.userId !== resourceUserId) {
-    if (itemId) {
-      throw new CustomErrors.NotFoundError(`No item found with id: ${itemId}`);
+    if (item) {
+      throw new CustomErrors.NotFoundError(`No item found with id: ${item}`);
     } else {
       throw new CustomErrors.NotFoundError(
         `No item found with id: ${resourceUserId}`
