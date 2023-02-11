18ad0a1bc08280e95ce1896f398fc1e58752e9b1
Andre Katov <andy.katov@icloud.com>
2023-02-11 23:57:17 +0200

    deleted file

--- errors/out-of-stock-error.js
+++
@@ -1,8 +0,0 @@
-import CustomErrorClass from "./custom-error-class.js";
-export class OutOfStockError extends CustomErrorClass {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.outOfStockItems = message;
-  }
-}
