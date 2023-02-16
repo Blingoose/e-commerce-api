aac7429f36d773f478bedc147047282d1a243158
Andre Katov <andy.katov@icloud.com>
2023-02-16 19:39:37 +0200

    minor change

--- controllers/productController.js
+++ controllers/productController.js
@@ -82,15 +82,15 @@ const productControllers = {
       result = result.select(fieldsList);
     }
 
-    // if (Object.keys(req.query).length === 0) {
-    //   result = Product.find(queryObject);
-    // } else {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
+    if (Object.keys(req.query).length === 0) {
+      result = Product.find(queryObject);
+    } else {
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 10;
+      const skip = (page - 1) * limit;
 
-    result = result.skip(skip).limit(limit);
-    // }
+      result = result.skip(skip).limit(limit);
+    }
 
     const products = await result;
 
