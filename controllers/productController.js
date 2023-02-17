1423142a5496c73ef35a80132411f00107855c55
Andre Katov <andy.katov@icloud.com>
2023-02-17 02:04:51 +0200

    improved functionality of req.query

--- controllers/productController.js
+++ controllers/productController.js
@@ -26,6 +26,7 @@ const productControllers = {
       company,
       search,
       sort,
+      sortOrder,
       fields,
       numericFilters,
     } = req.query;
@@ -54,25 +55,40 @@ const productControllers = {
         "<": "$lt",
         "<=": "$lte",
       };
+
+      // regex to match word boundaries of each one of the matching symbols <, <=, =, >=, >
       const regEx = /\b(<|<=|=|>=|>)\b/g;
-      let filters = numericFilters.replace(
-        regEx,
-        (match) => `-${operatorMap[match]}-`
-      );
-      const options = ["price", "rating"];
-      filters = filters.split(",").forEach((element) => {
-        const [field, operator, value] = element.split("-");
-        if (options.includes(field)) {
-          queryObject[field] = { [operator]: Number(value) };
+
+      // return an array. in cases when the user passes more than one numericFilters, split them by comma.
+      let filters = numericFilters.split(",").reduce((acc, filter) => {
+        // split every array item by the matching word boundaries, "price>20" will become ["price", ">", "20"]
+        const [field, operator, value] = filter.split(regEx);
+        if (["price", "averageRating"].includes(field)) {
+          acc[field] = { [operatorMap[operator]]: Number(value) };
         }
-      });
+
+        return acc;
+      }, {});
+
+      Object.assign(queryObject, filters);
     }
 
     let result = Product.find(queryObject);
 
+    let sortObj = {};
     if (sort) {
-      const sortList = sort.split(",").join(" ");
-      result = result.sort(sortList);
+      if (sort.split(",").length === 1) {
+        sortObj[sort] = sortOrder === "desc" ? -1 : 1;
+        result = result.sort(sortObj);
+      } else if (sort.split(",").length > 1) {
+        const sortArr = sort.split(",");
+        sortObj = sortArr.reduce((acc, field) => {
+          acc[field] = sortOrder === "desc" ? -1 : 1;
+
+          return acc;
+        }, {});
+        result = result.sort(sortObj);
+      }
     } else {
       result = result.sort("price");
     }
@@ -82,19 +98,18 @@ const productControllers = {
       result = result.select(fieldsList);
     }
 
-    if (Object.keys(req.query).length === 0) {
-      result = Product.find(queryObject);
-    } else {
-      const page = Number(req.query.page) || 1;
-      const limit = Number(req.query.limit) || 10;
-      const skip = (page - 1) * limit;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
-      result = result.skip(skip).limit(limit);
-    }
+    result = result.skip(skip).limit(limit);
 
     const products = await result;
 
-    res.status(StatusCodes.OK).json({ products, count: products.length });
+    res.status(StatusCodes.OK).json({
+      products,
+      count: products.length,
+    });
   }),
 
   getSingleProduct: asyncWrapper(async (req, res, next) => {
