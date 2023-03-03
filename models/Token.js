e742e709faaff92ef77a51ba3c75a6e6322ea3c5
Andre Katov <andy.katov@icloud.com>
2023-03-03 22:17:48 +0200

    added Token model for refreshToken functionality

---
+++ models/Token.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const TokenSchema = new mongoose.Schema(
+  {
+    refreshToken: { type: String, required: true },
+
+    ip: { type: String, required: true },
+
+    userAgent: { type: String, required: true },
+
+    isValid: { type: Boolean, default: true },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Token = mongoose.model("Token", TokenSchema);
+
+export default Token;
