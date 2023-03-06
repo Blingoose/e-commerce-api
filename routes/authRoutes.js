cf7ecf4cbd246c0d0732dfebbe24ed129f070cae
Andre Katov <andy.katov@icloud.com>
2023-03-06 19:20:59 +0200

    require user authentication on logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 import authControllers from "../controllers/authController.js";
 import express from "express";
+import { authenticateUser } from "../middleware/authentication-middleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", authControllers.register);
 authRouter.post("/login", authControllers.login);
-authRouter.get("/logout", authControllers.logout);
+authRouter.delete("/logout", authenticateUser, authControllers.logout);
 authRouter.get("/verify-email", authControllers.verifyEmail);
 
 export default authRouter;
