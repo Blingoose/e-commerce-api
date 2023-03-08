1c42ec9be32a6abefe4419a857e363aee4bb624e
Andre Katov <andy.katov@icloud.com>
2023-03-08 02:26:20 +0200

    added routes to handle forgot/reset password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,8 @@ authRouter.post("/register", authControllers.register);
 authRouter.post("/login", authControllers.login);
 authRouter.delete("/logout", authenticateUser, authControllers.logout);
 authRouter.get("/verify-email", authControllers.verifyEmail);
+authRouter.get("/reset-page", authControllers.resetPasswordPage);
+authRouter.post("/reset-password", authControllers.resetPassword);
+authRouter.post("/forgot-password", authControllers.forgotPassword);
 
 export default authRouter;
