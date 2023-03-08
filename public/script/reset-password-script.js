82074b50ca66f2a75b1074c5daa4ecc953be8fa1
Andre Katov <andy.katov@icloud.com>
2023-03-08 17:18:03 +0200

    few minor additions

--- public/script/reset-password-script.js
+++ public/script/reset-password-script.js
@@ -1,14 +1,25 @@
 const form = document.querySelector("#reset-password-form");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
+  let timeoutId;
   const message = document.querySelector(".message");
   const email = document.querySelector("#email").value;
   const token = document.querySelector("#token").value;
   const password = document.querySelector("#password").value;
   const repeatPassword = document.querySelector("#repeat-password").value;
+
   if (password !== repeatPassword) {
-    return (message.innerText = "Passwords do not match!");
+    message.innerText = "Passwords do not match!";
+    return;
   }
+
+  if (form.dataset.submitting === "true") {
+    // if form is already being submitted, do nothing
+    return;
+  }
+
+  form.dataset.submitting = "true"; // set submitting flag
+
   const data = { email, password, token };
 
   const dynamicURL = `${window.location.protocol}//${window.location.hostname}${
@@ -24,6 +35,16 @@ form.addEventListener("submit", async (e) => {
     },
     body: JSON.stringify(data),
   });
+
+  form.dataset.submitting = "false"; // unset submitting flag
+
   const result = await response.json();
   message.innerText = Object.values(result);
+  password.innerText = "";
+  repeatPassword.innerText = "";
+
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    message.innerText = "";
+  }, 3000);
 });
