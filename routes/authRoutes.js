832ef845c939242200d6ffb3f0c780eab2b264cb
Andre Katov <andy.katov@icloud.com>
2023-03-11 21:55:55 +0200

    added rateLimiter to reset-password route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,19 @@
 import authControllers from "../controllers/authController.js";
 import express from "express";
 import { authenticateUser } from "../middleware/authentication-middleware.js";
+import { rateLimiter } from "../utils/utils.js";
 
 const authRouter = express.Router();
+
+const fifteenMinutes = 15 * 16 * 1000;
+const limitErrorMesage = "Too many attempts, try again later";
+const limitRetries = rateLimiter(fifteenMinutes, 5, limitErrorMesage);
 
 authRouter.post("/register", authControllers.register);
 authRouter.post("/login", authControllers.login);
 authRouter.delete("/logout", authenticateUser, authControllers.logout);
 authRouter.get("/verify-email", authControllers.verifyEmail);
-authRouter.post("/reset-password", authControllers.resetPassword);
+authRouter.post("/reset-password", limitRetries, authControllers.resetPassword);
 authRouter.post("/forgot-password", authControllers.forgotPassword);
 
 export default authRouter;
